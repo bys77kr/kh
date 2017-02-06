834a8fa8c6ce2313d1326cd7096330765b657991
user2 <user2@KH_H>
2017-02-06 20:59:48 +0900

    t

--- testProject/src/testProject/TestProject.java
+++ testProject/src/testProject/TestProject.java
@@ -5,8 +5,6 @@ public class TestProject {
 	public static void main(String[] args) {
 		System.out.println("test");
 		
-		System.out.println("dddddddddddddddd");
-		System.out.println("tttttttttt");
 	}
 
 }
